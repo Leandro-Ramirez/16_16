2f1b147d436b27f239ad7365110e42f365a2b600
Leandro-Kun <90274325+Leandro-Ramirez@users.noreply.github.com>
2022-01-25 20:24:18 -0600

    Opción de ejercicio

---
+++ Informacion/Forms/frmInformacion.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Forms
+{
+    partial class frmInformacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInformacion));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lvRegistro = new System.Windows.Forms.ListView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbRegistrar = new System.Windows.Forms.ToolStripButton();
+            this.tslRegistrar = new System.Windows.Forms.ToolStripLabel();
+            this.tsbCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tslCancelar = new System.Windows.Forms.ToolStripLabel();
+            this.tsbCalcular = new System.Windows.Forms.ToolStripButton();
+            this.tslCalcular = new System.Windows.Forms.ToolStripLabel();
+            this.tsbSalir = new System.Windows.Forms.ToolStripButton();
+            this.tslSalir = new System.Windows.Forms.ToolStripLabel();
+            this.chNombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chEdad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSexo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(79, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(465, 33);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Informacion de los Trabajadores";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 45);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(67, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(88, 79);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(49, 16);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(85, 45);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(279, 21);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(143, 78);
+            this.txtEdad.Multiline = true;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 21);
+            this.txtEdad.TabIndex = 6;
+            // 
+            // lvRegistro
+            // 
+            this.lvRegistro.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chNombre,
+            this.chEdad,
+            this.chSexo});
+            this.lvRegistro.GridLines = true;
+            this.lvRegistro.Location = new System.Drawing.Point(13, 125);
+            this.lvRegistro.Name = "lvRegistro";
+            this.lvRegistro.Size = new System.Drawing.Size(592, 258);
+            this.lvRegistro.TabIndex = 7;
+            this.lvRegistro.UseCompatibleStateImageBehavior = false;
+            this.lvRegistro.View = System.Windows.Forms.View.Details;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbRegistrar,
+            this.tslRegistrar,
+            this.tsbCancelar,
+            this.tslCancelar,
+            this.tsbCalcular,
+            this.tslCalcular,
+            this.tsbSalir,
+            this.tslSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 386);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(618, 25);
+            this.toolStrip1.TabIndex = 8;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbRegistrar
+            // 
+            this.tsbRegistrar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbRegistrar.Image = ((System.Drawing.Image)(resources.GetObject("tsbRegistrar.Image")));
+            this.tsbRegistrar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRegistrar.Name = "tsbRegistrar";
+            this.tsbRegistrar.Size = new System.Drawing.Size(23, 22);
+            this.tsbRegistrar.Text = "Registrar";
+            this.tsbRegistrar.Click += new System.EventHandler(this.TsbRegistrar_Click);
+            // 
+            // tslRegistrar
+            // 
+            this.tslRegistrar.Name = "tslRegistrar";
+            this.tslRegistrar.Size = new System.Drawing.Size(53, 22);
+            this.tslRegistrar.Text = "Registrar";
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsbCancelar.Image")));
+            this.tsbCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(23, 22);
+            this.tsbCancelar.Text = "Cancelar";
+            this.tsbCancelar.Click += new System.EventHandler(this.TsbCancelar_Click);
+            // 
+            // tslCancelar
+            // 
+            this.tslCancelar.Name = "tslCancelar";
+            this.tslCancelar.Size = new System.Drawing.Size(53, 22);
+            this.tslCancelar.Text = "Cancelar";
+            // 
+            // tsbCalcular
+            // 
+            this.tsbCalcular.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCalcular.Image = ((System.Drawing.Image)(resources.GetObject("tsbCalcular.Image")));
+            this.tsbCalcular.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCalcular.Name = "tsbCalcular";
+            this.tsbCalcular.Size = new System.Drawing.Size(23, 22);
+            this.tsbCalcular.Text = "Calcular";
+            this.tsbCalcular.Click += new System.EventHandler(this.TsbCalcular_Click);
+            // 
+            // tslCalcular
+            // 
+            this.tslCalcular.Name = "tslCalcular";
+            this.tslCalcular.Size = new System.Drawing.Size(50, 22);
+            this.tslCalcular.Text = "Calcular";
+            // 
+            // tsbSalir
+            // 
+            this.tsbSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSalir.Image = ((System.Drawing.Image)(resources.GetObject("tsbSalir.Image")));
+            this.tsbSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalir.Name = "tsbSalir";
+            this.tsbSalir.Size = new System.Drawing.Size(23, 22);
+            this.tsbSalir.Text = "Salir";
+            this.tsbSalir.Click += new System.EventHandler(this.TsbSalir_Click);
+            // 
+            // tslSalir
+            // 
+            this.tslSalir.Name = "tslSalir";
+            this.tslSalir.Size = new System.Drawing.Size(29, 22);
+            this.tslSalir.Text = "Salir";
+            // 
+            // chNombre
+            // 
+            this.chNombre.Text = "Nombre";
+            this.chNombre.Width = 347;
+            // 
+            // chEdad
+            // 
+            this.chEdad.Text = "Edad";
+            this.chEdad.Width = 108;
+            // 
+            // chSexo
+            // 
+            this.chSexo.Text = "Sexo";
+            this.chSexo.Width = 133;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbFemenino);
+            this.groupBox1.Controls.Add(this.rbMasculino);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(370, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 74);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo:";
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(21, 33);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(96, 20);
+            this.rbMasculino.TabIndex = 10;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFemenino
+            // 
+            this.rbFemenino.AutoSize = true;
+            this.rbFemenino.Location = new System.Drawing.Point(123, 32);
+            this.rbFemenino.Name = "rbFemenino";
+            this.rbFemenino.Size = new System.Drawing.Size(94, 20);
+            this.rbFemenino.TabIndex = 11;
+            this.rbFemenino.TabStop = true;
+            this.rbFemenino.Text = "Femenino";
+            this.rbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // frmInformacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 411);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lvRegistro);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmInformacion";
+            this.Text = "Infromacion del Personal";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.ListView lvRegistro;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbRegistrar;
+        private System.Windows.Forms.ToolStripLabel tslRegistrar;
+        private System.Windows.Forms.ToolStripButton tsbCancelar;
+        private System.Windows.Forms.ToolStripLabel tslCancelar;
+        private System.Windows.Forms.ToolStripButton tsbCalcular;
+        private System.Windows.Forms.ToolStripLabel tslCalcular;
+        private System.Windows.Forms.ToolStripButton tsbSalir;
+        private System.Windows.Forms.ToolStripLabel tslSalir;
+        private System.Windows.Forms.ColumnHeader chNombre;
+        private System.Windows.Forms.ColumnHeader chEdad;
+        private System.Windows.Forms.ColumnHeader chSexo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.RadioButton rbMasculino;
+    }
+}
+
---
+++ Informacion/Forms/frmInformacion.cs
@@ -0,0 +1,129 @@
+﻿#region Formulario
+#region Usos
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+
+namespace Forms
+{
+    public partial class frmInformacion : Form
+    {
+        #region Variables Globales
+        int Masculino = 0;
+        int Femenino = 0;
+        int i;
+        int x = 0;
+        #endregion
+
+        //Finalizado
+        #region Inisializando Formulario Informacion
+        public frmInformacion()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        //Falta
+        #region Tool Strip Boton Registrar Click
+        private void TsbRegistrar_Click(object sender, EventArgs e)
+        {
+            string Nombre = txtNombre.Text;
+            int Edad = int.Parse(txtEdad.Text);
+            string Sexo = "";
+
+            if (rbMasculino.Checked == true) Sexo = "Masculino";
+            if (rbFemenino.Checked == true) Sexo = "Femenino";
+
+            if (Sexo == "")
+            {
+                MessageBox.Show("Debe Seleccionar su Sexo..!!", "Informacion");
+                return;
+            }
+
+            switch (Sexo)
+            {
+                case "Masculino": Masculino = Masculino+1;
+                    break;
+                case "Femenino": Femenino = Femenino+1;
+                    break;
+            }
+
+            ListViewItem Fila = new ListViewItem(Nombre);
+            Fila.SubItems.Add(Edad.ToString());
+            Fila.SubItems.Add(Sexo);
+
+            TsbCancelar_Click(sender, e);
+        }
+        #endregion
+
+        //Falta
+        #region Tool Strip Boton Calcular Click
+        private void TsbCalcular_Click(object sender, EventArgs e)
+        {
+            
+            for (i = 0; i < x ; i++)
+            {
+
+            }
+
+        }
+        #endregion
+
+        //Finalizado
+        #region Tool Strip Boton Cancelar Click
+        private void TsbCancelar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtEdad.Clear();
+            txtNombre.Focus();
+        }
+        #endregion
+
+        //Finalizado
+        #region Tool Strip Boton Salir Click
+        private void TsbSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("¿Estas Seguro que quieres salir?", "Informacion",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+            if (r == DialogResult.Yes) this.Close();
+        }
+        #endregion
+
+        #region Codigo por Agregar
+        //Edad Promedio:
+        //int EdadPromedio;
+        //int Edades = 0;
+        //Edades + Edad = Edades;
+        //Edades/x = EdadPromedio;
+
+        //Nombre del Profesor Más Joven:
+        //string ProfesorMasJoven = "";
+        //
+        //Nombre del Profesor Más Viejo:
+        //string ProfesorMasViejo = "";
+        //
+        //Numero de Profesoras que Tienen una Edad Mayor al Promedio:
+        //int ProfesorasMayorP = 0;
+        //
+        //Numero de Profesores que Tienen una Edad Menor al Promedio:
+        //int ProfesoresMenorP = 0;
+        //
+        //
+        //
+        //MessageBox.Show("La Edad Promedio de los Profesores es de: "+EdadPromedio+" Años", 
+        //"El Profesor Más Joven es: "+ProfesorMasJoven, 
+        //"El Profesor con Mayor Edad es: "+ProfesorMasViejo,
+        //"El Número de Profesoras Mayor al Promedio es: "+ProfesorasMayorP,
+        //"El Número de Profesores Menor al Promedio es: "+ProfesoresMenorP, MessageBoxButtons.Ok, MessageBoxIcon.---);
+        #endregion
+    }
+}
+#endregion
